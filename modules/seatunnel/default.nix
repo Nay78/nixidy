b6ad1710adf3e219ced4b728ac50413c188d82e1
alejg <nayguel.martinez@gmail.com>
2025-10-13 14:44:36 -0300

    44

--- modules/seatunnel/default.nix
+++ modules/seatunnel/default.nix
@@ -1,37 +1,34 @@
 { lib, ... }:
 {
-  # applications.k8s-gw-api-crds = {
-  #   yamls = [
-  #     (builtins.readFile seatunnel-crd)
-  #   ];
-  # };
-  # --set-string adminui.service.type=LoadBalancer https://seatunnel.io/downloads/stackgres-k8s/stackgres/latest/helm/stackgres-operator.tgz
+  #        error: The option `applications.seatunnel.resources.core.v1.Service.seatunnel.metadata.namespace` is defined both null and not null, in `/nix/store/5n18yk7slwnnw3yfq6di7fq761irnjk2-source/modules/applicati
+  # ons/yamls.nix' and `/nix/store/5n18yk7slwnnw3yfq6di7fq761irnjk2-source/modules/applications/helm.nix'.
+  # make: *** [makefile:8: p] Error 1
+
   applications.seatunnel = {
     namespace = "seatunnel";
     createNamespace = true;
-    # yamls = [
-    #   ''
-    #     apiVersion: v1
-    #     kind: Service
-    #     metadata:
-    #       name: seatunnel
-    #       namespace: seatunnel
-    #       annotations:
-    #         tailscale.com/expose: "true"
-    #     spec:
-    #       selector:
-    #         app: seatunnel
-    #       ports:
-    #         - protocol: TCP
-    #           port: 80
-    #           targetPort: 8443
-    #   ''
-    # ];
+    yamls = [
+      ''
+        apiVersion: v1
+        kind: Service
+        metadata:
+          name: tailscale
+          namespace: seatunnel
+          annotations:
+            tailscale.com/expose: "true"
+        spec:
+          selector:
+            app: seatunnel
+          ports:
+            - protocol: TCP
+              port: 80
+              targetPort: 5801
+      ''
+    ];
 
     # allowVolumeExpansion = true;
 
     helm.releases.seatunnel = {
-
       # helm pull oci://registry-1.docker.io/apache/seatunnel-helm --version ${VERSION}
       chart = lib.helm.downloadHelmChart {
         repo = "oci://registry-1.docker.io/apache";
@@ -40,9 +37,8 @@
         chartHash = "sha256-wKSaQMnENRFRY90INmsE0WlLOBIR1ehZCsiA5H+C1pY=";
       };
 
-      values = {
-
-      };
+      # values = {
+      # };
     };
 
   };
